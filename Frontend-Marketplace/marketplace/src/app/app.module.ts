ff2ebe32283b7497ce0ce5923fc25737d62f325d
Carolina Salvado <carolina.salvado7@gmail.com>
2023-04-12 18:35:55 +0100

    home - ligações e dropdowns feitos

--- Frontend-Marketplace/marketplace/src/app/app.module.ts
+++ Frontend-Marketplace/marketplace/src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { TermosCondicoesComponent } from './termos-condicoes/termos-condicoes.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { CestoComponent } from './cesto/cesto.component';
+
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'termos-condicoes', component: TermosCondicoesComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'cesto', component: CestoComponent },
+  
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    TermosCondicoesComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    CestoComponent,
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
---
+++ Frontend-Marketplace/marketplace/src/app/cesto/cesto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cesto',
+  templateUrl: './cesto.component.html',
+  styleUrls: ['./cesto.component.css']
+})
+export class CestoComponent {
+
+}
