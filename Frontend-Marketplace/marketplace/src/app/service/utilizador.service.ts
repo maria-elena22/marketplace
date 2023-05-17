158df39bd768b0ee3e39334ce9707a6797558060
maria-elena22 <mariaelenamun16@gmail.com>
2023-05-18 00:11:21 +0100

    Ip Atualizado

--- Frontend-Marketplace/marketplace/src/app/service/utilizador.service.ts
+++ Frontend-Marketplace/marketplace/src/app/service/utilizador.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse, HttpParams, HttpErrorResponse} from "@angular/common/http";
 import { Observable, throwError } from 'rxjs';
 import { catchError, map,tap } from 'rxjs/operators';
-import { SignUpDTO } from '../model/models';
+import { NotificacaoDTO, SignUpDTO } from '../model/models';
 
 
 
@@ -18,18 +18,48 @@ export class UtilizadorService {
 
     const headers = new HttpHeaders();
     
-    // https://grupo12.pt:8080/api/utilizador/login?email=cont%40gmail.com&password=Cont%401234
+    // https://34.30.176.39:8080/api/utilizador/login?email=cont%40gmail.com&password=Cont%401234
 
-    const url = `https://grupo12.pt:8080/api/utilizador/login?email=${email}&password=${password}`;
+    const url = `https://34.30.176.39:8080/api/utilizador/login?email=${email}&password=${password}`;
     console.log(url)
     return this.http.get<any>(url, { headers, observe: 'response'});
 
 
   }
 
+  getNotificacoes(){
+    const token = localStorage.getItem('jwt_token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+    const url = `https://34.30.176.39:8080/api/notificacao`;
+
+    return this.http.get<Array<NotificacaoDTO>>(url ,{ headers, observe: 'response' })
+      .pipe(
+          catchError((error) => {
+          console.log('An error occurred:', error);
+          return throwError('Something went wrong');
+        })
+      );
+  }
+
+  getNotificacoesNum(){
+    const token = localStorage.getItem('jwt_token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+    const url = `https://34.30.176.39:8080/api/notificacao/num`;
+
+    return this.http.get<number>(url ,{ headers, observe: 'response' })
+      .pipe(
+          catchError((error) => {
+          console.log('An error occurred:', error);
+          return throwError('Something went wrong');
+        })
+      );
+  }
+
   insertConsumidor(consumidor: SignUpDTO): Observable<HttpResponse<any>> {
     const headers = new HttpHeaders();
-    const url = `https://grupo12.pt:8080/api/utilizador/register/consumidor`;
+    const url = `https://34.30.176.39:8080/api/utilizador/register/consumidor`;
     console.log(url)
     return this.http.post<any>(url, consumidor, { headers, observe: 'response' });
 
@@ -37,7 +67,7 @@ export class UtilizadorService {
 
   insertFornecedor(fornecedor: SignUpDTO){
     const headers = new HttpHeaders();
-    const url = `https://grupo12.pt:8080/api/utilizador/register/fornecedor`;
+    const url = `https://34.30.176.39:8080/api/utilizador/register/fornecedor`;
     console.log(url)
     return this.http.post<any>(url, fornecedor, { headers, observe: 'response' });
 
@@ -47,7 +77,7 @@ export class UtilizadorService {
 
     const token = localStorage.getItem('jwt_token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    const url = 'https://grupo12.pt:8080/api/utilizador/detalhes';
+    const url = 'https://34.30.176.39:8080/api/utilizador/detalhes';
 
     return this.http.get<any>(url, { headers, observe: 'response' })
       .pipe(
