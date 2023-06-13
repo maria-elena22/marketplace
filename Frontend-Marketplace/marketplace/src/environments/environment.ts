31c65b1d5526635533a489948373ba2d01232f60
maria-elena22 <mariaelenamun16@gmail.com>
2023-06-13 23:42:44 +0100

    url corrigido

--- Frontend-Marketplace/marketplace/src/environments/environment.ts
+++ Frontend-Marketplace/marketplace/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //backendUrl: 'https://grupo12.pt:8080/api' 
-    backendUrl: 'https://localhost:8080/api'
+    backendUrl: 'https://grupo12.pt:8080/api' 
+    //backendUrl: 'https://localhost:8080/api'
   };
